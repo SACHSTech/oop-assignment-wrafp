4d0e48fe2e3b4202c7f66cdbefba50e5005b714c
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-09 11:00:37 -0500

    added constructor to Restaurant.java

--- src/FoodDelivery/Restaurant.java
+++ src/FoodDelivery/Restaurant.java
@@ -6,6 +6,11 @@ public class Restaurant {
     // Instance Variables
     private String placeName;
 
+    // Constructor
+    public Restaurant(String thePlaceName) {
+        this.placeName = thePlaceName;
+    }
+
     // Methods
     public String getPlaceName() {
         return placeName;
