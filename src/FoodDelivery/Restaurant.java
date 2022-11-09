36448c70c4e58e5b406d7b56401d4a39d1a59b03
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-09 10:58:03 -0500

    Added instance var and get method in Restaurant

--- src/FoodDelivery/Restaurant.java
+++ src/FoodDelivery/Restaurant.java
@@ -3,4 +3,12 @@ import FoodDelivery.*;
 
 public class Restaurant {
     
+    // Instance Variables
+    private String placeName;
+
+    // Methods
+    public String getPlaceName() {
+        return placeName;
+    }
+
 }
