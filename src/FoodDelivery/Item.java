ad1d8c4fd12272d501fde63db12e0e1904d11f28
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-10 10:14:29 -0500

    Added getter and setters for Item.java

--- src/FoodDelivery/Item.java
+++ src/FoodDelivery/Item.java
@@ -3,4 +3,22 @@ import FoodDelivery.*;
 
 public class Item {
     
+    // Instance variables
+    private String name;
+    private double price;
+    private boolean isBought;
+
+    // Methods
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean setIsBought() {
+        isBought = true;
+        return isBought;
+    }
 }
