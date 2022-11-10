1e9d49254b4da694708b49b0b7ef5dff0d15527f
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-10 10:42:19 -0500

    Made Item.java an abstract class

--- src/FoodDelivery/Item.java
+++ src/FoodDelivery/Item.java
@@ -1,12 +1,18 @@
 package FoodDelivery;
 import FoodDelivery.*;
 
-public class Item {
+public abstract class Item {
     
     // Instance variables
     private String name;
     private double price;
     private boolean isBought;
+
+    // Constructor
+    public Item(String theName, double thePrice) {
+        this.name = theName;
+        this.price = thePrice;
+    }
 
     // Methods
     public String getName() {
