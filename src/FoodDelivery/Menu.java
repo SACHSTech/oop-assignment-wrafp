a04aafa60ba51bbe008f7df217c7e72d64c2746f
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-11 10:33:01 -0500

    Added Arraylist in Menu.java for holding objects

--- src/FoodDelivery/Menu.java
+++ src/FoodDelivery/Menu.java
@@ -1,20 +1,23 @@
 package FoodDelivery;
 import FoodDelivery.*;
+import java.util.ArrayList;
 
 public class Menu {
 
+    // Arraylist for holding objects of Item objects
+    ArrayList<Item> menuItems = new ArrayList<>();
+
     // Instance Variables
-    private int[] numItems;
+    private int numItems;
 
     // Constructor
-    public Menu(int[] theNumItems) {
+    public Menu(int theNumItems) {
         this.numItems = theNumItems;
     }
 
     // Methods
     public int getNumItems() {
-        return numItems.length;
+        return this.numItems;
     }
-
 
 }
