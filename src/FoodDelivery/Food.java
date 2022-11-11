25a150d2eac607fe0e92ddc9f5438be537be32e2
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-11 10:42:06 -0500

    Added instance variables to Food.java

--- src/FoodDelivery/Food.java
+++ src/FoodDelivery/Food.java
@@ -3,4 +3,9 @@ import FoodDelivery.*;
 
 public class Food extends Item {
     
+    // Instance variables
+    private boolean isSpicy;
+    private boolean isVegan;
+    private boolean isGlutenFree;
+    
 }
