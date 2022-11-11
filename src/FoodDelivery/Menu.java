a01108fca758418cb4a9f2e7dc14ddc868e2f11d
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-11 10:13:53 -0500

    Added constructor to Menu.java

---
+++ src/FoodDelivery/Menu.java
@@ -0,0 +1,15 @@
+package FoodDelivery;
+import FoodDelivery.*;
+
+public class Menu {
+
+    // Instance Variables
+    private int[] numItems;
+
+    // Constructor
+    public Menu(int[] theNumItems) {
+        this.numItems = theNumItems;
+    }
+
+
+}
