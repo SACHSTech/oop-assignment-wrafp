6284c5bd62734788eda21202462c71e9a4b27461
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-14 10:48:36 -0500

    Added constructor to Drink.java

--- src/FoodDelivery/Drink.java
+++ src/FoodDelivery/Drink.java
@@ -6,5 +6,13 @@ public class Drink extends Item {
     // Instance variables
     private int size;
     private boolean isHot;
-    
+
+    // Constructor
+    public Drink(String theName, int thePrice, int theSize, boolean theIsHot) {
+        super(theName, thePrice);
+        this.size = theSize;
+        this.isHot = theIsHot;
+    }
+
+    // Methods
 }
