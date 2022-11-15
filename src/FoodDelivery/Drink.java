b186bfeaab2b14b08906c18e613201c1bb0c9c89
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-15 10:00:11 -0500

    Added getSize() method to Drink.java

--- src/FoodDelivery/Drink.java
+++ src/FoodDelivery/Drink.java
@@ -4,15 +4,20 @@ import FoodDelivery.*;
 public class Drink extends Item {
     
     // Instance variables
-    private int size;
+    private String size;
     private boolean isHot;
 
     // Constructor
-    public Drink(String theName, int thePrice, int theSize, boolean theIsHot) {
+    public Drink(String theName, int thePrice, String theSize, boolean theIsHot) {
         super(theName, thePrice);
         this.size = theSize;
         this.isHot = theIsHot;
     }
 
     // Methods
+    public String getSize() {
+        return size;
+    }
+
+    
 }
