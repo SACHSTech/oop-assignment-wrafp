e45642b56045c26e19786570955be7d6ef5081ce
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-18 22:02:09 -0500

    Added javadocs to Food.java

--- src/FoodDelivery/Food.java
+++ src/FoodDelivery/Food.java
@@ -12,7 +12,14 @@ public class Food extends Item {
     private boolean isVegan;
     private boolean isGlutenFree;
 
-    // Constructor
+    /**
+     * Constructor - creates new instance of food object
+     * @param theName - the name of the food
+     * @param thePrice - the price of the food
+     * @param theIsSpicy - if the food is spicy
+     * @param theIsVegan - if the food is vegan
+     * @param theIsGlutenFree - if the food is gluten-free
+     */
     public Food(String theName, double thePrice, boolean theIsSpicy, boolean theIsVegan, boolean theIsGlutenFree) {
         super(theName, thePrice); // temporarily static to test functionality, will fix later - super(getName(), getPrice());
         this.isSpicy = theIsSpicy;
@@ -20,19 +27,30 @@ public class Food extends Item {
         this.isGlutenFree = theIsGlutenFree;
     }
 
-    // Methods
+    /**
+     * @return if the food is spicy
+     */
     public boolean getIsSpicy() {
         return isSpicy;
     }
 
+    /**
+     * @return if the food is vegan
+     */
     public boolean getIsVegan() {
         return isVegan;
     }
 
+    /**
+     * @return if the food is gluten-free
+     */
     public boolean getIsGlutenFree() {
         return isGlutenFree;
     }
 
+    /**
+     * @return food with all its properties (name, price, spiciness, vegan friendly, gluten-free)
+     */
     public String toString() {
         return super.getName() + " | Price: $" + super.getPrice() + " |\nAttributes: Spicy: " + isSpicy + " | Vegan: " + isVegan + " | Gluten-Free: " + isGlutenFree + " |\n";  
     }
