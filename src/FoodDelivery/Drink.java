9f07c037431e9e104167b5c89bc0cfc69df0ff3a
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-18 21:59:17 -0500

    Added javadocs to Drink.java

--- src/FoodDelivery/Drink.java
+++ src/FoodDelivery/Drink.java
@@ -10,17 +10,27 @@ public class Drink extends Item {
     // Instance variables
     private boolean isHot;
 
-    // Constructor
+    /**
+     * Constructor - creates new instance of drink object
+     * @param theName - the name of the drink
+     * @param thePrice - the price of the drink
+     * @param theIsHot - if the drink is hot/cold
+     */
     public Drink(String theName, int thePrice, boolean theIsHot) {
         super(theName, thePrice);
         this.isHot = theIsHot;
     }
 
-    // Methods
+    /**
+     * @return if the drink is hot/cold
+     */
     public boolean getIsHot() {
         return isHot;
     }
 
+    /**
+     * @return drink with all its properties (name, price, hot/cold)
+     */
     public String toString() {
         return super.getName() + " | Price: $" + super.getPrice() + "\n Hot: " + isHot + "\n";  
     }
