446354f4ea4addb017cb954468bc5b8cf8878577
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-11-18 22:03:53 -0500

    Added javadocs to Item.java

--- src/FoodDelivery/Item.java
+++ src/FoodDelivery/Item.java
@@ -6,21 +6,31 @@ import FoodDelivery.*;
  * @author: K.huang
  */
 public class Item {
+
     // Instance variables
     private String name;
     private double price;
 
-    // Constructor
+    /**
+     * Constructor - creates new instance of Item object
+     * @param theName - the name of the item (food/drink)
+     * @param thePrice - the price of the item (food/drink)
+     */
     public Item(String theName, double thePrice) {
         this.name = theName;
         this.price = thePrice;
     }
 
-    // Methods
+    /**
+     * @return the name of the food/drink
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the name of the food/drink
+     */
     public double getPrice() {
         return price;
     }
